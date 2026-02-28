ff666c593e918e7b00eae9cd470379b2912289b1
Egor Galkin <egor04012002@icloud.com>
2026-02-28 14:54:00 +0300

    Initial Commit

---
+++ Travel app/Travel_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_appApp.swift
+//  Travel app
+//
+//  Created by Егор Галкин on 28.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Travel_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
